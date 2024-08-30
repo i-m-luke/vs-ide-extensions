47fb347246732cdeb9769a627ebc812ff0625ec2
Lukáš Kratochvíl <kratochvil.luke@gmail.com>
2024-08-30 14:00:15 +0200

    Add project files.

---
+++ FirstExtension/Commands/MyCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace FirstExtension.Commands
+{
+    [Command(PackageIds.FirstCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            // Zobrazení messageboxu: await VS.MessageBox.ShowWarningAsync("FirstExtension", "Button clicked");
+            await VS.MessageBox.ShowWarningAsync("FIRST MESSAGE BOX", "...");
+
+            var docView = await VS.Documents.GetActiveDocumentViewAsync();
+            if (docView is null)
+            {
+                return;
+            }
+
+            var selection = docView.TextView?.Selection.SelectedSpans.FirstOrDefault();
+            if (selection is null)
+            {
+                return;
+            }
+
+            docView.TextBuffer?.Replace(selection.Value, Guid.NewGuid().ToString());
+        }
+    }
+}
